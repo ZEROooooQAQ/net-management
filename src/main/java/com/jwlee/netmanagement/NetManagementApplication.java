f92a36282a612b041cfbf5403abba9c9c88f9370
ZEROooooQAQ <1920273845@qq.com>
2019-08-17 16:13:09 +0800

    create net management project

---
+++ src/main/java/com/jwlee/netmanagement/NetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jwlee.netmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetManagementApplication.class, args);
+    }
+
+}
